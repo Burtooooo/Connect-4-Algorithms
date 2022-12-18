f52701ed9674263a946208969360083b518c12ea
John Wahlig <27918605+johnwahlig@users.noreply.github.com>
2022-12-17 23:50:22 -0500

    Added successor method to Game
    This is for MCTS, to get successor states without modifying the game

--- Game.py
+++ Game.py
@@ -31,6 +31,13 @@ class Game:
             if self.board[i][col] == 0:
                 return i
 
+    def successor(self, col, piece):
+        successor_board = self.board.copy()
+        for i in range(ROW_COUNT):
+            if self.board[i][col] == 0:
+                self.board[i][col] = piece
+                return
+        return successor_board
 
     def get_valid_locations(self):
         valid_locations = []
